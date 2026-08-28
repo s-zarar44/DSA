66d6e8be7ce5245c286e142e63ee8f489da4c012
s_zarar <zararmajeed100@gmail.com>
2026-08-29 02:07:11 +0530

    Sync Codeforces: 1696B - NIT Destroys the Universe [Java 21]

--- Codeforces/1696B_NIT_Destroys_the_Universe.java
+++ Codeforces/1696B_NIT_Destroys_the_Universe.java
@@ -1,31 +1,34 @@
 import java.util.Scanner;
+ 
 public class Problem_1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+ 
         int t = sc.nextInt();
-        for (int test = 1; test <= t; test++) {
+ 
+        while (t-- > 0) {
             int n = sc.nextInt();
  
             int count = 0;
-            int i = 0;
+            boolean inSegment = false;
  
-            while (i < n) {
+            for (int i = 0; i < n; i++) {
                 int curr = sc.nextInt();
-                if (curr != 0) {
+ 
+                if (curr == 0) {
+                    inSegment = false;
+                } else if (!inSegment) {
                     count++;
-                    i++;
-                    while (i < n) {
-                        int next = sc.nextInt();
-                        if (next == 0) break;
-                        i++;
-                    }
+                    inSegment = true;
                 }
-                i++;
             }
  
-            if (count == 0) System.out.println(0);
-            else if (count == 1) System.out.println(1);
-            else System.out.println(2);
+            if (count == 0)
+                System.out.println(0);
+            else if (count == 1)
+                System.out.println(1);
+            else
+                System.out.println(2);
         }
     }
 }
