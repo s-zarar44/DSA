90a4e0b64451d2ab11d7cb258710f255d2911679
s_zarar <zararmajeed100@gmail.com>
2026-08-29 03:31:04 +0530

    Sync Codeforces: 1675B - Make It Increasing [Java 21]

---
+++ Codeforces/1675B_Make_It_Increasing.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Problem_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int test = 1; test <= t; test++) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+ 
+            int count = 0;
+ 
+            if (n == 1) {
+                System.out.println(count);
+            } else {
+                for (int i = n-2; i >= 0; i--) {
+                    if (arr[i+1] == 0) {
+                        count = -1;
+                        break;
+                    }
+                    while (arr[i] >= arr[i+1]) {
+                        arr[i] /= 2;
+                        count++;
+                    }
+                }
+                System.out.println(count);
+            }
+        }
+    }
+}
