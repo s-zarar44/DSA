98a98b2685d85f4a6559f731a70a92838e2c5cac
s_zarar <zararmajeed100@gmail.com>
2026-08-29 17:09:03 +0530

    Sync Codeforces: 1666D - Deletive Editing [Java 21]

--- Codeforces/1666D_Deletive_Editing.java
+++ Codeforces/1666D_Deletive_Editing.java
@@ -1,37 +1,61 @@
 import java.util.Scanner;
+ 
 public class Problem_1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        for (int test = 1; test <= n; test++) {
  
+        int n = sc.nextInt();
+ 
+        while (n-- > 0) {
             String s = sc.next();
             String t = sc.next();
  
             int[] freqS = new int[26];
             int[] freqT = new int[26];
  
-            for (int i = 0; i < s.length(); i++) {
-                freqS[s.charAt(i) - 'A']++;
+            for (char ch : s.toCharArray()) {
+                freqS[ch - 'A']++;
             }
  
-            for (int i = 0; i < t.length(); i++) {
-                freqT[t.charAt(i) - 'A']++;
+            for (char ch : t.toCharArray()) {
+                freqT[ch - 'A']++;
             }
  
-            int i = 0;
+            boolean possible = true;
  
-            while (i < s.length()) {
-                int ind = s.charAt(i) - 'A';
-                if (freqS[ind] > freqT[ind]) {
-                    freqS[ind]--;
-                    s = s.replaceFirst(s.charAt(i) + "", "");
-                } else {
-                    i++;
+            for (int i = 0; i < 26; i++) {
+                if (freqS[i] < freqT[i]) {
+                    possible = false;
+                    break;
                 }
             }
  
-            System.out.println(t.contentEquals(s) ? "YES" : "NO");
+            if (!possible) {
+                System.out.println("NO");
+                continue;
+            }
+ 
+            int[] remove = new int[26];
+ 
+            for (int i = 0; i < 26; i++) {
+                remove[i] = freqS[i] - freqT[i];
+            }
+ 
+            StringBuilder remaining = new StringBuilder();
+ 
+            for (char ch : s.toCharArray()) {
+                int idx = ch - 'A';
+ 
+                if (remove[idx] > 0) {
+                    remove[idx]--;
+                } else {
+                    remaining.append(ch);
+                }
+            }
+ 
+            System.out.println(remaining.toString().equals(t) ? "YES" : "NO");
         }
+ 
+        sc.close();
     }
 }
