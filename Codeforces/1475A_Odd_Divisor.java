6fbeb81e12dd8f558c97b1bb350369af20547e53
s_zarar <zararmajeed100@gmail.com>
2026-09-14 22:26:37 +0530

    Sync Codeforces: 1475A - Odd Divisor [Java 21]

---
+++ Codeforces/1475A_Odd_Divisor.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Problem_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            long n = sc.nextLong();
+            while (n % 2 == 0) {
+                n /= 2;
+            }
+            System.out.println(n > 1 ? "YES" : "NO");
+        }
+    }
+}
